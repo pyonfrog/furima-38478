a1821359d8a5e5b42a89125930ac7a85518000b2
pyonfrog <trick_810@yahoo.co.jp>
2023-02-03 22:23:38 +0900

    新規登録・ログイン・ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :firstname,           presence: true
+  validates :lastname,            presence: true
+  validates :pronounce_firstname, presence: true
+  validates :pronounce_lastname,  presence: true
+  validates :nickname,            presence: true
+  validates :birthday,            presence: true
 end
