62ea10dd24968c7cf22971623af59b3e74054dd4
pyonfrog <trick_810@yahoo.co.jp>
2023-02-04 22:13:59 +0900

    テストコード実装

--- Gemfile
+++ Gemfile
@@ -67,3 +67,9 @@ gem 'devise'
 gem 'mail', '2.7.1'
 
 gem 'rspec-rails', '~> 4.0.0'
+
+gem 'factory_bot_rails'
+
+gem 'faker'
+
+gem 'pry-rails'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,52 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "name, email, password, password_confirmationが存在すれば登録できること" do
-    user = build(:user)
-    expect(user).to be_valid
-  end
-
-  it "nameが存在しない場合は登録できないこと" do
-    user = build(:user, name: nil)
+  it "nicknameが存在しない場合は登録できないこと" do
+    user = FactoryBot.build(:user, nickname: nil)
     user.valid?
-    expect(user.errors[:name]).to include("を入力してください")
+    expect(user.errors.full_messages).to include("Nickname can't be blank")
   end
 
   it "emailが存在しない場合は登録できないこと" do
-    user = build(:user, email: nil)
+    user = FactoryBot.build(:user, email: nil)
     user.valid?
-    expect(user.errors[:email]).to include("を入力してください")
+    expect(user.errors.full_messages).to include("Email can't be blank")
   end
 
   it "passwordが存在しない場合は登録できないこと" do
-    user = build(:user, password: nil)
+    user = FactoryBot.build(:user, password: nil)
     user.valid?
-    expect(user.errors[:password]).to include("を入力してください")
+    expect(user.errors.full_messages).to include("Password can't be blank")
   end
 
-  it "passwordが存在しても、password_confirmationが存在しない場合は登録できないこと" do
-    user = build(:user, password_confirmation: "")
+  it "passwordとpassword_confirmationが一致していないと登録できない" do
+    user = FactoryBot.build(:user, password_confirmation: "")
     user.valid?
-    expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+    expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+  end
+
+  it "lastnameが存在しない場合は登録できないこと" do
+    user = FactoryBot.build(:user, lastname: nil)
+    user.valid?
+    expect(user.errors.full_messages).to include("Lastname can't be blank")
+  end
+
+  it "firstnameが存在しない場合は登録できないこと" do
+    user = FactoryBot.build(:user, firstname: nil)
+    user.valid?
+    expect(user.errors.full_messages).to include("Firstname can't be blank")
+  end
+
+  it "pronounce_lastnameが存在しない場合は登録できないこと" do
+    user = FactoryBot.build(:user, pronounce_lastname: nil)
+    user.valid?
+    expect(user.errors.full_messages).to include("Pronounce lastname can't be blank")
+  end
+
+  it "pronounce_firstnameが存在しない場合は登録できないこと" do
+    user = FactoryBot.build(:user, pronounce_firstname: nil)
+    user.valid?
+    expect(user.errors.full_messages).to include("Pronounce firstname can't be blank")
+  end
+
+  it "birthdayが存在しない場合は登録できないこと" do
+    user = FactoryBot.build(:user, birthday: nil)
+    user.valid?
+    expect(user.errors.full_messages).to include("Birthday can't be blank")
   end
 
   it "重複したemailが存在する場合は登録できないこと" do
-    user = create(:user)
-    another_user = build(:user, email: user.email)
+    user = FactoryBot.create(:user)
+    another_user = FactoryBot.build(:user, email: user.email)
     another_user.valid?
-    expect(another_user.errors[:email]).to include("はすでに存在します")
+    expect(another_user.errors.full_messages).to include("Email has already been taken")
   end
 
-  it "passwordが7文字以上であれば登録できること" do
-    user = build(:user, password: "0000000", password_confirmation: "0000000")
-    user.valid?
-    expect(user).to be_valid
-  end
-
-  it "passwordが6文字以下であれば登録できないこと" do
-    user = build(:user, password: "000000", password_confirmation: "000000")
-    user.valid?
-    expect(user.errors[:password]).to include("は7文字以上で入力してください")
+  it "passwordが5文字以下であれば登録できないこと" do
+    password = "aa11"
+      user = FactoryBot.build(:user, password: password, password_confirmation: password)
+      user.valid?
+      expect(user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
   end
 
 end
