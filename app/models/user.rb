b1133f8526cb44f0f32a21373d08fa80e920c76a
pyonfrog <trick_810@yahoo.co.jp>
2023-02-06 20:59:16 +0900

    バリデーション・テストコードの追加・修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
   
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-  
-  validates :firstname,           presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :lastname,            presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+
+  validates :firstname,           presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :lastname,            presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :pronounce_firstname, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :pronounce_lastname,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :nickname,            presence: true
