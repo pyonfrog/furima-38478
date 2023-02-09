551347b75520aebb8e2f60fa9a5b0f7b41d0863e
pyonfrog <trick_810@yahoo.co.jp>
2023-02-09 19:21:58 +0900

    Active Hashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,12 @@ class Item < ApplicationRecord
   belongs_to       :user
   has_one          :purchased_item
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :derivery_charge
+  belongs_to :sender_area
+  belongs_to :daystosend
+
 end
