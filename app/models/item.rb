a62681bc78101c3c8c00d145686e0c1f61fa8965
pyonfrog <trick_810@yahoo.co.jp>
2023-02-09 21:31:56 +0900

    アソシエーション・バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,14 @@ class Item < ApplicationRecord
   belongs_to :sender_area
   belongs_to :daystosend
 
+  validates :name,               :string,     presence: true
+  validates :explanat,           :text,       presence: true
+  validates :price,              :integer,    presence: true, numericality: {greater_than: 300,less_than: 9999999}, format: { with: /\A[0-9]+\z/ }
+  validates :category_id,        :integer,    numericality: { other_than: 1 , message: "can't be blank" }
+  validates :status_id,          :integer,    numericality: { other_than: 1 , message: "can't be blank" }
+  validates :derivery_charge_id, :integer,    numericality: { other_than: 1 , message: "can't be blank" }
+  validates :sender_area_id,     :integer,    numericality: { other_than: 1 , message: "can't be blank" }
+  validates :daystosend_id,      :integer,    numericality: { other_than: 1 , message: "can't be blank" }
+  validates :user,               :references, presence: true, foreign_key: true
+
 end
