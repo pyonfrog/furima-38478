db4898c7262c9230fecb9efbd172358abf763fab
pyonfrog <trick_810@yahoo.co.jp>
2023-02-19 22:08:27 +0900

    ビュー・コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,7 @@ class ItemsController < ApplicationController
  before_action :authenticate_user!, only: [:new]
 
  def index
-  @item = Item.order("created_at DESC")
-  @items = Item.all
+  @items = Item.order(:id)
  end
 
  def new
