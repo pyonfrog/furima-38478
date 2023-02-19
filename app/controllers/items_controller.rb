b98907c9b18841b5cdb9a466e0b05d3beadf6e1b
pyonfrog <trick_810@yahoo.co.jp>
2023-02-19 21:24:02 +0900

    商品一覧表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,8 @@ class ItemsController < ApplicationController
  before_action :authenticate_user!, only: [:new]
 
  def index
+  @item = Item.order("created_at DESC")
+  @items = Item.all
  end
 
  def new
