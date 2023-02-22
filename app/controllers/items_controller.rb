40fce15798a0c438816a2d27bbf537bc5ef3ae3c
pyonfrog <trick_810@yahoo.co.jp>
2023-02-22 21:14:46 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
  before_action :authenticate_user!, only: [:new, :edit]
+ before_action :set_item, only: [:show, :edit, :update]
 
  def index
   @items = Item.order(created_at: :desc)
@@ -19,23 +20,24 @@ class ItemsController < ApplicationController
  end
 
 def show
-  @item = Item.find(params[:id])
 end
 
 def edit
-  @item = Item.find(params[:id])
   unless current_user.id == @item.user_id
     redirect_to root_path
   end
 end
 
 def update
-  @item = Item.find(params[:id])
   if @item.update(item_params)
     redirect_to item_path(params[:id])
   else
     render :edit
   end
+end
+
+def set_item
+  @item = Item.find(params[:id])
 end
 
 
